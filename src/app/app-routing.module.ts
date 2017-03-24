b85b9273edda166a79f25c6757bb0a5fb3650705
Tamazlykar Daniil <tadani7@gmail.com>
2017-03-24 15:37:25 +0700

    Added Routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {DevelopViewComponent} from './develop-view/develop-view.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/develop', pathMatch: 'full' },
+  { path: 'editor',  component: AppComponent },
+  { path: 'develop',     component: DevelopViewComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DevelopViewComponent } from './develop-view/develop-view.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevelopViewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
