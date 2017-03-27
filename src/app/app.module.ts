fe065bae0120f3dd4be1b6488b7d8279e9e9594d
Tamazlykar Daniil <tadani7@gmail.com>
2017-03-27 17:24:28 +0700

    Added Material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { DevelopViewComponent } from './develop-view/develop-view.component';
-import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import {AppRoutingModule} from "./app-routing.module";
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
