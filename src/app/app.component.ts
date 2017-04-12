215e003defb4145bfe0ce89b4c222e535f2b804f
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-13 01:53:04 +0700

    added priting state and sidenav throught store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { MdSidenav } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
-import { State, getSidenavState } from './shared/reducers';
+import {
+  State,
+  getSidenavState,
+  getUserId,
+  getProjectId,
+  getDiagramId,
+  getModelElementId,
+  getViewElementId
+} from './shared/reducers';
 import * as ui from './shared/actions/ui-state';
+import * as app from './shared/actions/app-state';
+
+import { AuthService, ProjectService } from './shared/services';
 
 @Component({
   selector: 'uml-root',
@@ -15,7 +26,21 @@ export class AppComponent implements OnInit {
   @ViewChild(MdSidenav)
   private mdSidenavComponent: MdSidenav;
 
-  constructor(private store: Store<State>) {}
+  constructor(private store: Store<State>, private authService: AuthService, private projectService: ProjectService) {
+    this.authService.login('tadani7@gmail.com', '07021995');
+
+    this.printAppStates();
+
+    // select 1 project in list
+    this.projectService.projects.subscribe((projects: any[]) => {
+      if (projects.length === 0) {
+        return;
+      }
+      console.log('App: projects', projects);
+      const projectId = projects[0].$key;
+      this.projectService.set(projectId);
+    });
+  }
 
   public ngOnInit() {
     this.store.select(getSidenavState)
@@ -34,5 +59,15 @@ export class AppComponent implements OnInit {
 
   private open() {
     this.mdSidenavComponent.open();
+  }
+
+
+
+  public printAppStates() {
+    this.store.select(getUserId).subscribe(val => console.log('Store: userId', val));
+    this.store.select(getProjectId).subscribe(val => console.log('Store: projectId', val));
+    this.store.select(getDiagramId).subscribe(val => console.log('Store: diagramId', val));
+    this.store.select(getModelElementId).subscribe(val => console.log('Store: modelElementId', val));
+    this.store.select(getViewElementId).subscribe(val => console.log('Store: viewElementId', val));
   }
 }
