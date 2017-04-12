5df3ed707f65eed5648b3a2ce98c5ebc79516c14
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-12 13:41:15 +0700

    init sidenav component

---
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewChild } from '@angular/core';
+
+
+@Component({
+    selector: 'uml-sidenav',
+    templateUrl: 'sidenav.component.html',
+    styleUrls: ['sidenav.component.css']
+})
+export class SidenavComponent {
+}
