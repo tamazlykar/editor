d775ade08c34c6694eb63674e931427f5636778f
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-12 13:41:40 +0700

    init element explorer

---
+++ src/app/shared/components/sidebar/element-explorer/element-explorer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'uml-element-explorer',
+    templateUrl: 'element-explorer.component.html',
+    styleUrls: ['element-explorer.component.css']
+})
+export class ElementExplorerComponent {
+  constructor() { }
+}
