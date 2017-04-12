06608f7d42431fc6d7c31ec522ae2ce65963696c
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-12 13:18:34 +0700

    remove unnececery files

--- src/app/graphics/element-visitor.ts
+++
@@ -1,8 +0,0 @@
-import {Text, Rectangle, Line} from "./elements";
-import {Params} from "./types";
-
-export abstract class ElementVisitor {
-  public abstract visitText(element: Text, params?: Params)
-  public abstract visitRectangle(element: Rectangle, params?: Params);
-  public abstract visitLine(element: Line, params?: Params);
-}
