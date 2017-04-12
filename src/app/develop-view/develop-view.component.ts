4561aad051b1908c2e80bde06b58f7e693c4900e
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-12 13:40:15 +0700

    developer component update

--- src/app/develop-view/develop-view.component.ts
+++ src/app/develop-view/develop-view.component.ts
@@ -1,29 +1,59 @@
 import { Component, AfterViewInit } from '@angular/core';
-import {Graphics} from "../graphics/graphics";
+import { DiagramController } from '../diagram-controller/diagram-controller';
+import { Graphics } from '../graphics/graphics';
+import { Class as PMClass } from '../model/presentation-model/classifier/class';
+import { ProjectService, AuthService } from '../shared/services';
+
 
 @Component({
-  selector: 'app-develop-view',
+  selector: 'uml-develop-view',
   templateUrl: './develop-view.component.html',
   styleUrls: ['./develop-view.component.css']
 })
 export class DevelopViewComponent implements AfterViewInit {
-
-  constructor() { }
-
   private g: Graphics;
+
+  constructor(private authService: AuthService, private projectService: ProjectService) {
+    this.test();
+  }
+
+  public test() {
+    this.authService.login('tadani7@gmail.com', '07021995');
+    this.projectService.projects.subscribe(val => console.log(val));
+  }
+
 
   public ngAfterViewInit(): void {
     const element = document.getElementById('canvas');
-    this.g = new Graphics(element, 300, 300);
+    this.g = new Graphics(element, 800, 600);
     this.testing();
   }
 
   private testing() {
     let text = this.g.text(50, 50, "Hello world!");
     let text2 = this.g.text(100, 100, 'Hello man');
-    let set = this.g.set();
-    set.add(text);
-    set.add(text2);
-    set.draggable(true);
+    let set1 = this.g.set();
+    set1.add(text);
+    set1.add(text2);
+    set1.draggable();
+
+    let rect = this.g.rect(300, 300, 100, 200);
+    let text3 = this.g.text(350, 350, 'ClassName');
+    let line = this.g.line(600, 200, 800, 200);
+    console.log('Line x1', line.x1);
+    console.log('Line', line);
+    let set2 = this.g.set();
+    set2.add(rect);
+    set2.add(text3);
+    set2.add(line);
+    set2.draggable();
+
+    //let line = this.g.line(600, 200, 800, 200);
+    // let set3 = this.g.set();
+    // set3.add(line);
+    // set3.draggable(true);
+
+    const pmClass = new PMClass();
+    const dc = new DiagramController([pmClass]);
   }
 }
