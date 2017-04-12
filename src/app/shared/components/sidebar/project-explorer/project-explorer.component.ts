d411d0c05e5a5ca82ca71565be720d8413b1820c
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-12 13:42:27 +0700

    init project-explorer component

---
+++ src/app/shared/components/sidebar/project-explorer/project-explorer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'uml-project-explorer',
+    templateUrl: 'project-explorer.component.html',
+    styleUrls: ['project-explorer.component.css']
+})
+export class ProjectExplorerComponent {
+  constructor() {}
+}
