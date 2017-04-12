49ddabd7606983b118a7dbb4e0ffdfaf20c2a238
Tamazlykar Daniil <tadani7@gmail.com>
2017-04-12 13:48:54 +0700

    app component and module update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MdSidenav } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { State, getSidenavState } from './shared/reducers';
+import * as ui from './shared/actions/ui-state';
 
 @Component({
-  selector: 'app-root',
+  selector: 'uml-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  private sidenavOpened$: Observable<boolean>;
+  @ViewChild(MdSidenav)
+  private mdSidenavComponent: MdSidenav;
+
+  constructor(private store: Store<State>) {}
+
+  public ngOnInit() {
+    this.store.select(getSidenavState)
+      .subscribe((state: boolean) => {
+        if (!state) {
+          return;
+        }
+        this.open();
+      });
+
+    this.mdSidenavComponent.onClose
+      .subscribe(() => {
+        this.store.dispatch(new ui.CloseSidenavAction());
+      });
+  }
+
+  private open() {
+    this.mdSidenavComponent.open();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,66 @@ import { MaterialModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import 'hammerjs';
 
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from './shared/reducers';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+import { FirebaseService } from './shared/db.service';
+
 import { AppComponent } from './app.component';
 import { DevelopViewComponent } from './develop-view/develop-view.component';
+
+import { ToolbarComponent } from './shared/components/toolbar';
+import { SidenavComponent } from './shared/components/sidenav';
+import { SidebarComponent } from './shared/components/sidebar';
+import { ElementExplorerComponent } from './shared/components/sidebar/element-explorer';
+import { ProjectExplorerComponent } from './shared/components/sidebar/project-explorer';
+import { InstrumentalPanelComponent } from './shared/components/instrumental-panel';
+
+import { AuthService, FirebaseAuthService } from './shared/services/auth';
+import { ProjectService, FirebaseProjectService } from './shared/services/project';
+
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCIp2-7wO-aD2Y2eoD0_z6GC4qnXGGrEgM',
+    authDomain: 'uml-editor-89c08.firebaseapp.com',
+    databaseURL: 'https://uml-editor-89c08.firebaseio.com',
+    projectId: 'uml-editor-89c08',
+    storageBucket: 'uml-editor-89c08.appspot.com',
+    messagingSenderId: '729173763'
+  };
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevelopViewComponent
+    DevelopViewComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    SidebarComponent,
+    ElementExplorerComponent,
+    ProjectExplorerComponent,
+    InstrumentalPanelComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    StoreModule.provideStore(appReducer),
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
-  providers: [],
+  providers: [
+    FirebaseService,
+    FirebaseProjectService,
+    { provide: AuthService, useClass: FirebaseAuthService },
+    { provide: ProjectService, useClass: FirebaseProjectService  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
